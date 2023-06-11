173504aaf5be60994044631d017fdda31adc9888
OkisSienah <104500247+OkisSienah@users.noreply.github.com>
2023-06-11 17:22:39 +0700

    SQL

---
+++ dbTable.sql
@@ -0,0 +1,33 @@
+create table user(
+    userid int primary key auto_increment not null,
+    fname varchar(60),
+    bdate varchar(60),
+    gender varchar(8),
+    address varchar(60),
+    username varchar(60),
+    password varchar(60)
+);
+
+create table order(
+    order_id int primary key auto_increment not null,
+    userid int(11),
+    q_noriAS int(10),
+    q_noriIT int(10),
+    q_noriTM int(10),
+    q_noriR int(10),
+    total int(60)
+);
+
+alter table order add foreign key(userid)  references user(userid);
+
+create table cart(
+    userid int(11),
+    q_noriAS int(10),
+    q_noriIT int(10),
+    q_noriTM int(10),
+    q_noriR int(10),
+    total int(60)
+);
+
+alter table cart add foreign key(userid)  references user(userid);
+
