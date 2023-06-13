d63f9383ff3c8386e2b96db30915b9d8c3beac9f
OkisSienah <104500247+OkisSienah@users.noreply.github.com>
2023-06-13 14:24:38 +0700

    SQL

--- dbTable.sql
+++ dbTable.sql
@@ -10,24 +10,20 @@ create table user(
 
 create table order_log(
     order_id int primary key auto_increment not null,
-    userid int(11),
+    user varchar(30),
     q_noriAS int(10),
     q_noriIT int(10),
     q_noriTM int(10),
     q_noriR int(10),
-    total int(60)
+    total int(60),
+    buy_time timestamp
 );
-
-alter table order add foreign key(userid)  references user(userid);
 
 create table cart(
-    userid int(11),
+    user varchar(30),
     q_noriAS int(10),
     q_noriIT int(10),
     q_noriTM int(10),
-    q_noriR int(10),
-    total int(60)
+    q_noriR int(10)
 );
-
-alter table cart add foreign key(userid)  references user(userid);
 
